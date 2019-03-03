6d36f2ecfb1d299473814861a0e101b417b9241d
pepe_witch <skpepe134@gmail.com>
2019-03-03 11:09:33 +0700

    use onSnapshot event to reduce database reading when user get into landing page

--- src/index.ts
+++ src/index.ts
@@ -13,22 +13,41 @@ admin.initializeApp({
 const firestore = new Firestore();
 const campersRef = firestore.collection("campers");
 
+let summary;
+
+campersRef.onSnapshot(next => {
+  const data = next.docs.map(doc => doc.data());
+  summary = {
+    content: data.filter(e => e.submitted && e.major === "content").length,
+    marketing: data.filter(e => e.submitted && e.major === "marketing").length,
+    design: data.filter(e => e.submitted && e.major === "design").length,
+    programming: data.filter(e => e.submitted && e.major === "programming")
+      .length
+  };
+});
+
 export const campers = functions.https.onRequest((req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
-  campersRef
-    .get()
-    .then(snapshot => {
-      const data = snapshot.docs.map(doc => doc.data());
-      res.status(200).send({
-        content: data.filter(e => e.submitted && e.major === "content").length,
-        marketing: data.filter(e => e.submitted && e.major === "marketing")
-          .length,
-        design: data.filter(e => e.submitted && e.major === "design").length,
-        programming: data.filter(e => e.submitted && e.major === "programming")
-          .length
+  if (summary) {
+    return res.status(200).send(summary);
+  } else {
+    campersRef
+      .get()
+      .then(snapshot => {
+        const data = snapshot.docs.map(doc => doc.data());
+        res.status(200).send({
+          content: data.filter(e => e.submitted && e.major === "content")
+            .length,
+          marketing: data.filter(e => e.submitted && e.major === "marketing")
+            .length,
+          design: data.filter(e => e.submitted && e.major === "design").length,
+          programming: data.filter(
+            e => e.submitted && e.major === "programming"
+          ).length
+        });
+      })
+      .catch(e => {
+        res.sendStatus(500);
       });
-    })
-    .catch(e => {
-      res.sendStatus(500);
-    });
+  }
 });
